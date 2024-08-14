50a2101013aaec6bb1d0a3eadd6495cf1adadb76
braun1604 <braunda1997@gmail.com>
2024-08-14 07:43:50 +0700

    HW-2.16 new

--- loginPage.js
+++ loginPage.js
@@ -1,13 +1,14 @@
+import { renderComments } from "./renderComments.js";
 export const renderLogin = () => {
-    const loginTodo = document.createElement('div');
-    loginTodo.innerHTML =  `<div class="login">
+  const loginTodo = document.createElement("div");
+  loginTodo.innerHTML = `<div class="login">
 <div>
   <h2>Форма входа</h2>
 </div>
 <div class="form-row">
-<input type="text" id="login-input" class="input" placeholder="Введите логин">
+<input type="text" value="admin" id="login-input" class="input" placeholder="Введите логин">
 <br />
-<input type="text" id="password-input" class="input" placeholder="Пароль">
+<input type="text" value="admin" id="password-input" class="input" placeholder="Пароль">
 </div>
 <div class="login-button-div">
 <button class="login-button" id="login-button"><b>Войти</b></button>
@@ -16,78 +17,54 @@ export const renderLogin = () => {
 <a href="index.html" id="link-to-tasks" class="registration">Зарегистрироваться</a>
 </div>
 `;
-const loginAddForm = document.getElementById("loginForm");
-loginAddForm.appendChild(loginTodo);
-const loginEl = document.getElementById("login-input");
-const passwordEl = document.getElementById("password-input");
-const loginButton = document.getElementById("login-button");
-loginButton.addEventListener("click", function (e) {
-  const loginElement = loginEl.value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;");
-  const passwordElement = passwordEl.value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;");
- 
-  // const commentHidden = document.querySelector(".add-form");
-  // commentHidden.style.display = "none";
-  // const loadingCommentsHidden = document.querySelector(".loading-comments");
-  // const loadingComments = (loadingCommentsHidden) => {
-  //   loadingCommentsHidden.style.display = "flex";
-  // };
-  // loading(loadingCommentsHidden);
+  const loginAddForm = document.getElementById("loginForm");
+  loginAddForm.appendChild(loginTodo);
+  const loginEl = document.getElementById("login-input");
+  const passwordEl = document.getElementById("password-input");
+  const loginButton = document.getElementById("login-button");
+  loginButton.addEventListener("click", function (e) {
+    const loginElement = loginEl.value
+      .replaceAll("&", "&amp;")
+      .replaceAll("<", "&lt;")
+      .replaceAll(">", "&gt;")
+      .replaceAll('"', "&quot;");
+    const passwordElement = passwordEl.value
+      .replaceAll("&", "&amp;")
+      .replaceAll("<", "&lt;")
+      .replaceAll(">", "&gt;")
+      .replaceAll('"', "&quot;");
 
-  fetch('https://wedev-api.sky.pro/api/user/login', {
-    method: "POST",
-    body: JSON.stringify({
-      login: loginElement,
-    password: passwordElement,
-    }),
-  })
-    .then((responce) => {
-      if (responce.status === 400) {
-        throw new Error("Пользователь с таким логином уже сущетсвует");
-      }
-      if (responce.status === 500) {
-        throw new Error("Сервер упал");
-      } else {
-        return responce.json();
-      }
+    fetch("https://wedev-api.sky.pro/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({
+        login: loginElement,
+        password: passwordElement,
+      }),
     })
-    .then(() => {
-      return fetch('https://wedev-api.sky.pro/api/user/login');
-    })
-    .then((responseData) => {
-      return responseData.json();
-    })
-    .then((response) => {
-      comments = response.comments;
-      renderComments(comments);
-    })
-    // .then(() => {
-    //   const loadingCommentsHidden = document.querySelector(".loading-comments");
-    //   loadingCommentsHidden.style.display = "none";
-    //   commentHidden.style.display = "flex";
-    //   inputEl.value = "";
-    //   textareaEl.value = "";
-    // })
-    .catch((error) => {
-      if (error.message == "400 error") {
-        alert("Пользователь с таким логином уже сущетсвует");
-      } else if (error.message == "Сервер упал") {
-        alert("Сервер сломался, попробуй позже");
-      } else {
-        error.message == "Failed to fetch";
-        alert("Кажется, у вас сломался интернет, попробуйте позже");
-      }
-      // loadingCommentsHidden.style.display = "none";
-      // commentHidden.style.display = "flex";
-    });
+      .then((responce) => {
+        if (responce.status === 400) {
+          throw new Error("Передан неправильный логин или пароль");
+        }
+        if (responce.status === 500) {
+          throw new Error("Сервер упал");
+        } else {
+          return responce.json();
+        }
+      })
 
-  renderComments(comments);
-});
-}
+      .then((response) => {
+        const user = response.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        if (error.message == "400 error") {
+          alert("Передан неправильный логин или пароль");
+        } else if (error.message == "Сервер упал") {
+          alert("Сервер сломался, попробуй позже");
+        } else {
+          error.message == "Failed to fetch";
+          alert("Кажется, у вас сломался интернет, попробуйте позже");
+        }
+      });
+  });
+};
--- registration.js
+++ registration.js
@@ -9,7 +9,7 @@ export const registration = () => {
 <br />
 <input type="text" id="form-login" class="input" placeholder="Введите логин">
 <br />
-<input type="text" id="form-password" class="input" placeholder="Введите пароль">
+<input type="password" id="form-password" class="input" placeholder="Введите пароль">
 </div>
 <div class="login-button-div">
 <button class="login-button" id="button-registration"><b>Зарегистрироваться</b></button>
@@ -20,5 +20,61 @@ export const registration = () => {
 `
     const registrationFormNew = document.getElementById("container");
     registrationFormNew.appendChild(registrationForm);
-};
 
+    const formNameEl = document.getElementById("form-name");
+    const formLoginEl = document.getElementById("form-login");
+    const formPasswordEl = document.getElementById("form-password");
+    const buttonRegistration = document.getElementById("button-registration");
+    buttonRegistration.addEventListener("click", function (e) {
+      const formNameElement = formNameEl.value
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;");
+        const formPasswordElement = formPasswordEl.value
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;");
+      const formLoginElement = formLoginEl.value
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;");
+  
+      fetch("https://wedev-api.sky.pro/api/user", {
+        method: "POST",
+        body: JSON.stringify({
+          login: formLoginElement,
+          name: formNameElement,
+          password: formPasswordElement,
+        }),
+      })
+        .then((responce) => {
+          if (responce.status === 400) {
+            throw new Error("Пользователь с таким логином уже сущетсвует");
+          }
+          if (responce.status === 500) {
+            throw new Error("Сервер упал");
+          } else {
+            return responce.json();
+          }
+        })
+  
+        .then((response) => {
+          const user = response.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          if (error.message == "400 error") {
+            alert("Пользователь с таким логином уже сущетсвует");
+          } else if (error.message == "Сервер упал") {
+            alert("Сервер сломался, попробуй позже");
+          } else {
+            error.message == "Failed to fetch";
+            alert("Кажется, у вас сломался интернет, попробуйте позже");
+          }
+        });
+    });
+  };
+  
