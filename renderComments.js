e8a6b99d46f7076e51880658211cb181d86775af
braun1604 <braunda1997@gmail.com>
2024-08-23 23:48:40 +0700

    HW - 2.17 +

--- renderComments.js
+++ renderComments.js
@@ -1,4 +1,5 @@
 import { initEventListener } from "./helper.js";
+import { format } from "date-fns"
 
 export const renderComments = (comments, user) => {
   const ulComments = document.createElement("ul");
@@ -8,19 +9,20 @@ export const renderComments = (comments, user) => {
   const commentsEl = document.getElementById("comments");
   const commentsHtml = comments
     .map((commentary, index) => {
-      const dateObject = new Date(`${commentary.date}`);
-      const day = dateObject.getDate().toString().padStart(2, "0");
-      const month = (dateObject.getMonth() + 1).toString().padStart(2, "0");
-      const year = dateObject.getFullYear();
-      const hours = dateObject.getHours().toString().padStart(2, "0");
-      const minutes = dateObject.getMinutes().toString().padStart(2, "0");
-      const seconds = dateObject.getSeconds().toString().padStart(2, "0");
-      const formattedDate = `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+      // const dateObject = new Date(`${commentary.date}`);
+      // const day = dateObject.getDate().toString().padStart(2, "0");
+      // const month = (dateObject.getMonth() + 1).toString().padStart(2, "0");
+      // const year = dateObject.getFullYear();
+      // const hours = dateObject.getHours().toString().padStart(2, "0");
+      // const minutes = dateObject.getMinutes().toString().padStart(2, "0");
+      // const seconds = dateObject.getSeconds().toString().padStart(2, "0");
+      // const formattedDate = `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+      const createDate = format( new Date(`${commentary.date}`), 'yyyy-MM-dd hh.mm.ss');
       const classButton = commentary.isLiked ? "-active-like" : "";
       return `<li class="comment" >
       <div class="comment-header" >
         <div data-index="${index}">${commentary.author.name}</div>
-        <div>${formattedDate}</div>
+        <div>${createDate}</div>
       </div>
       <div class="comment-body">
         <div class="comment-text" data-index="${index}">
